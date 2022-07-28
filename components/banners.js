6f7bc1556b7fcad1c0a3e87a68846a92d50d710d
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-07-28 10:39:47 +0100

    scrollable bar

--- components/banners.js
+++ components/banners.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
+import { Paper, Stack, List, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -9,27 +8,42 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
+  border: 'solid',
+  borderWidth: '1.5px',
 }));
 
 export default function Banners() {
   return (
     <div>
-      <Stack id="stack"
-        direction='row'
-              spacing={{ xs: 1, sm: 2, md: 4 }}
-              mt='20px'
+      <Stack
+        id="stack"
+        direction="row"
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        mt="20px"
+        sx={{ maxWidth: 375, overflow: 'auto', height: 50 }}
+        alignItems="center"
       >
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
+        <Item>Pizza</Item>
+        <Item>Later</Item>
+        <Item>Hamb</Item>
+        <Item>Halal</Item>
+        <Item>Halal</Item>
+        <Item>Halal</Item>
+        <Item>Halal</Item>
+        <Item>Halal</Item>
       </Stack>
-
+      {/* <Paper style={{ maxWidth: 300, overflow: 'auto' }} >
+   
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Paper> */}
     </div>
   );
 }

0894e4cbec1d573c4f527e74a5a7dffa91c9d87c
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-07-28 13:34:54 +0100

    Chips

--- components/recCard.js
+++ components/recCard.js
@@ -3,54 +3,52 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { IconButton, CardActionArea, CardActions, Stack, Chip } from '@mui/material';
+import {
+  IconButton,
+  CardActionArea,
+  CardActions,
+  Stack,
+  Chip,
+} from '@mui/material';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import GradeIcon from '@mui/icons-material/Grade';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
-
-
-
-export default function RecCard({data}) {
-
-
-
+export default function RecCard({ data }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={data[0].image}
-          alt=""
-        />
+        <CardMedia component="img" height="140" image={data[0].image} alt="" />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {data[0].title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-           {data[0].description}
+            {data[0].description}
           </Typography>
-          <Stack id="rating" direction="row" spacing={0.75} mt="3px">
-          <Chip icon={<GradeIcon/>} label="text" variant="outlined" />
-    
-          
-
-          {/* <GradeIcon fontSize="medium"/>
-          <Typography>
-            {data[0].rating}
-          </Typography> */}
+          <Stack spacing={2} alignItems="flex-start">
+            <Chip
+              label={data[0].rating}
+              icon={<GradeIcon />}
+              variant="outlined"
+              size="small"
+              sx={{ mt: 2, border: 'none' }}
+            />
+            <Chip
+              label={data[0].timeToCreate}
+              icon={<AccessTimeIcon />}
+              variant="outlined"
+              size="small"
+              sx={{ mt: 2, border: 'none' }}
+            />
           </Stack>
-          <Typography>
-            {data[0].timeToCreate} 
-          </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-      <IconButton variant="plain">
-        <FavoriteBorder />
-      </IconButton>
+        <IconButton variant="plain">
+          <FavoriteBorder />
+        </IconButton>
       </CardActions>
     </Card>
   );
 }
-

88357361a33b1fece91009a4bb3e4cc0923133ec
ivonivanova <ivon.b.ivanova@gmail.com>
2022-08-01 14:03:18 +0100

    additional files commented, back to hydration

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,21 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+// potential hydration fix
 
-// Create a theme instance.
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#556cd6',
-		},
-		secondary: {
-			main: '#19857b',
-		},
-		error: {
-			main: red.A400,
-		},
-	},
-});
+// import { createTheme } from '@mui/material/styles';
+// import { red } from '@mui/material/colors';
 
-export default theme;
+// // Create a theme instance.
+// const theme = createTheme({
+// 	palette: {
+// 		primary: {
+// 			main: '#556cd6',
+// 		},
+// 		secondary: {
+// 			main: '#19857b',
+// 		},
+// 		error: {
+// 			main: red.A400,
+// 		},
+// 	},
+// });
+
+// export default theme;

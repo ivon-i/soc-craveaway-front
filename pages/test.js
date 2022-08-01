a26335adabbb44d8deb1541ad48a4466c4421d2a
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-08-01 15:06:14 +0100

    created and finished new recipe card

--- pages/test.js
+++ pages/test.js
@@ -3,14 +3,19 @@ import { Box } from '@mui/system';
 import Image from 'next/image';
 import GradeIcon from '@mui/icons-material/Grade';
 import Rating from '@mui/material/Rating';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PeopleIcon from '@mui/icons-material/People';
+import PaymentIcon from '@mui/icons-material/Payment';
 import { useState } from 'react';
 
 export default function Test() {
-  const [value, setValue] = useState(4);
+  const [value, setValue] = useState(5);
   return (
-    <div>
-      <Typography> Grilled Chicken and Broc</Typography>
-      <Box width="200px" height="200px" backgroundColor="grey">
+    <Box ml="15px">
+      <Typography mt="30px" fontWeight={700}>
+        Grilled Chicken and Broc
+      </Typography>
+      <Box width="340px" height="200px" backgroundColor="grey" mt="20px">
         IMAGE GOES HERE
       </Box>
       <Stack direction="column">
@@ -19,47 +24,62 @@ export default function Test() {
           icon={<GradeIcon />}
           variant="outlined"
           size="small"
-          sx={{ mt: 2, border: 'none' }}
+          sx={{ mt: 2, border: 'none', width: '60px' }}
         />
         <Chip
-          label="4.5"
-          icon={<GradeIcon />}
+          label="20min"
+          icon={<AccessTimeIcon />}
           variant="outlined"
           size="small"
-          sx={{ mt: 2, border: 'none' }}
+          sx={{ mt: 2, border: 'none', width: '60px', ml: '4px' }}
         />
         <Chip
-          label="4.5"
-          icon={<GradeIcon />}
+          label="2"
+          icon={<PeopleIcon />}
           variant="outlined"
           size="small"
-          sx={{ mt: 2, border: 'none' }}
+          sx={{ mt: 2, border: 'none', width: '60px' }}
+        />
+        <Chip
+          label="£5"
+          icon={<PaymentIcon />}
+          variant="outlined"
+          size="small"
+          sx={{ mt: 2, border: 'none', width: '60px' }}
         />
       </Stack>
-      <Typography>Ingredients</Typography>
+      <Typography fontWeight={700} mt="20px">
+        Ingredients
+      </Typography>
       <Chip
-        label="dadadadad"
-        variant="filled"
+        label="Chicken"
+        variant="outlined"
         sx={{
-          color: 'white',
-          backgroundColor: '#000080',
+          borderColor: '#FCC62E',
+          borderWidth: '1.5px',
+          mt: '10px',
         }}
       ></Chip>
-      <Typography>Description</Typography>
+      <Typography fontWeight={700} mt="20px">
+        Description
+      </Typography>
       <Typography
-        sx={{ borderRadius: '8px', width: ' 80%', ml: '30px' }}
+        sx={{
+          borderRadius: '8px',
+          width: ' 80%',
+          display: 'inline-block',
+          whiteSpace: 'pre-line',
+        }}
         variant="outlined"
-        // onChange={AddIngredients}
         placeholder="Add Description"
-        rows={5}
+        mt="20px"
       >
-        {' '}
         This salmon salad recipe makes a main dish salad with chopped
         vegetables, flaky fish, and tangy citrus dressing. It has plenty of
         protein and healthy fats, so it’s nutritious and delicious!{' '}
       </Typography>
-      <Typography>Creator</Typography>
+      <Typography mt="20px">Creator</Typography>
       <Rating name="read-only" value={value} />
-    </div>
+    </Box>
   );
 }

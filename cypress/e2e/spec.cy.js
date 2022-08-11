b2c2e1fb4dec184037727cd85711252927108cbe
ivonivanova <ivon.b.ivanova@gmail.com>
2022-08-11 16:09:53 +0100

    search and auth0 half test added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,7 @@
 
 describe('The Home Page', () => {
   it('visits site', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('https://craveaway.vercel.app/');
   });
   it('clicks the hamburger menu', () => {
     cy.get('.burger-menu').click();
@@ -36,3 +36,37 @@ describe('The Home Page', () => {
 //     );
 //   });
 // });
+
+//generic visit test 
+it('visits site', () => {
+    cy.visit('https://craveaway.vercel.app/');
+  });
+  
+  //searching displays correct info
+  
+  describe('Search recipe', () => {
+    it('tests the search feature', () => {
+      cy.visit('https://craveaway.vercel.app/');
+      cy.get('input').type('keto').should('have.value', 'keto');
+      cy.get('[data-testid="SearchIcon"]').click();
+      cy.get(':nth-child(1) > .MuiPaper-root > .mui-style-19midj6').contains('Keto');
+    });
+  });
+  
+  describe('Submits a recipe', () => {
+  it('logs in through auth0', () => {
+      cy.visit('https://craveaway.vercel.app/')
+      cy.get('.burger-menu').click()
+      cy.get('.mui-style-p4gg3y > :nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiListItemText-primary > .MuiTypography-root').click();
+      
+      cy.origin("https://dev-659k2jkc.us.auth0.com/", () => {
+          cy.get("#username").type(Cypress.env("USERNAME"));
+          cy.get("#password").type(Cypress.env("PASSWORD"));
+          cy.contains("Continue").click();
+          });
+    });
+  
+    //once auth0 test works, should fill out each input field in createrecipe and submit
+  
+  });
+  

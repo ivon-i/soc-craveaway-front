9ed8d78348a5ce8d61202ffda98061ac9c0c25ca
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-08-22 14:48:46 +0100

    TimeOut

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,5 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default handleAuth();
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' });
+}
